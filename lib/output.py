ad673ad94eb07219d77014bc975e1ba7d7c3153a
Chester de Alday <codewithpatch@gmail.com>
2024-10-05 09:55:49 +0800

    Ignore output folder and add autocreate of output folder

--- lib/output.py
+++ lib/output.py
@@ -4,6 +4,7 @@ from pyspark.sql import DataFrame
 
 
 def write_df_to_csv(df: DataFrame, output_dir: Path, node_name: str):
+    output_dir.mkdir(parents=True, exist_ok=True)
     output_csv = output_dir / f"TABLE_{node_name}.csv"
 
     df.write.csv(str(output_csv), header=True, sep="\t")
